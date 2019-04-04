81c5925ca60aa69a63a0609600ca143097ad315a
Rhor <hor.raphaell@gmail.com>
2019-04-04 19:08:32 +0200

    testNormaliseString

--- www/assets/js/pageDeezer.js
+++ www/assets/js/pageDeezer.js
@@ -1,9 +1,10 @@
 const levenshtein = require('js-levenshtein');
 document.addEventListener("DOMContentLoaded", function(event) {
-    track = document.querySelectorAll('h2')[0].textContent.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/ /g,'');
+    track = document.querySelectorAll('h2')[0].textContent;
+    artist = document.querySelectorAll('h2')[1].textContent;
 
-
-    artist = document.querySelectorAll('h2')[1].textContent.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/ /g,'');
+    track = stringNormalize(track);
+    artist = stringNormalize(artist);
     console.log(track);
     console.log(artist);
 
@@ -13,7 +14,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var chaineValide = document.querySelector('input');
         var answer 
         if (chaineValide.value.length != 0){
-            answer = chaineValide.value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/ /g,'');
+            answer = chaineValide.value;
+            answer = stringNormalize(answer);
             console.log('answer :' + answer) 
             
             answerIsOk(track,answer,'track')
@@ -30,7 +32,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
     }
 
-
+    let stringNormalize = (string)=>{
+        return string.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/ /g,'');
+    }
 
     
 
